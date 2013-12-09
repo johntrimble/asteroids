1f1b5646e4b23133f70d6276137424bb1e22c908
John Trimble <trimblej@gmail.com>
2013-12-08 23:56:03 -0700

    Moved source files under clj directories in preperation for clojurescript support.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject asteroids "1.0.0-SNAPSHOT"
   :description "Asteroids"
 ;  :jvm-opts ["-Xmx3g"]
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :source-paths ["src/clj"]
+  :test-paths ["test/clj"])
