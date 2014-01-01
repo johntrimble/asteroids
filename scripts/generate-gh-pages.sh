a0377365bad53d456cf7e20b31c425903e86ad96
John Trimble <trimblej@gmail.com>
2014-01-01 16:50:07 -0700

    Added script to update gh-pages with build.

---
+++ scripts/generate-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+lein clean
+lein cljsbuild once prod
+TMP=`mktemp -d /tmp/asteroids.XXXXXX`
+git clone git@github.com:johntrimble/asteroids.git "$TMP"
+GIT="git --git-dir $TMP/.git --work-tree $TMP "
+$GIT checkout gh-pages
+$GIT rm -r .
+cp -r resources/public/* "${TMP}"
+$GIT add -A
+$GIT commit -m "Site update"
+$GIT push
+rm -rf "$TMP"
